8e7aabdbd5af55cf4057eda20faf7d9d6c88142a
ParvesPR <parves2585@gmail.com>
2022-04-17 06:49:44 +0800

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Pages/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
+      <Routes>
+        <Route></Route>
+      </Routes>
      
     </div>
   );
