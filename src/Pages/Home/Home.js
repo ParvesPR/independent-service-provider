3e698eb6d5172591803a548646b9f97a3a5ae557
ParvesPR <parves2585@gmail.com>
2022-04-17 23:26:24 +0800

    json file added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './Home.css';
-import banner from '../../images/banner.jpg'
 
 const Home = () => {
     return (
         <div id="banner" className='banner'>
-         <h1 className='banner-title'>Make Your Wedding a wonderfull story</h1>
+         <span className='banner-title'>Make Your Wedding A Wonderfull Story</span>
         </div>
     );
 };
